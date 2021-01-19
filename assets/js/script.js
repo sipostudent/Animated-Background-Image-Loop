89b935999bba1e7c3a0fe9ba562af29a4050a0c9
Sipo Charles <sipocharles@yahoo.co.uk>
2021-01-19 04:09:17 +0000

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Animated Background Logic
 
-var banner = document.getElementsByClassName('banner')[0];
-var blocks = document.getElementsByClassName('blocks');
-for (var i = 1; i < 400; i++) {
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+for (let i = 1; i < 400; i++) {
   banner.innerHTML += "<div class='blocks'></div>";
   blocks[i].style.animationDelay = `${i * 0.05}s`;
 }
